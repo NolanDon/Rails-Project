d1435c60b3012049930ba9c84dfb76ec765dc00b
nolan295 <nolan796@live.ca>
2019-12-17 21:51:21 +0000

    Remove whitespace in test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -6,7 +6,6 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
-
     10.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
