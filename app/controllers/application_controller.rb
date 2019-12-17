3e277ec251296ee694ab872985b1005d5a24b161
nolan295 <nolan796@live.ca>
2019-12-17 00:41:13 +0000

    Fixed user trying to find deleted user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,9 +28,11 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
-  
+
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
   end
   helper_method :current_user
 
