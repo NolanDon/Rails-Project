7bf265fe51d1abd3a4db8b35425f662b1cc20362
nolan295 <nolan796@live.ca>
2019-12-19 01:11:40 +0000

    Dry up code

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,7 +2,7 @@ class LineItem < ActiveRecord::Base
 
   belongs_to :order
   belongs_to :product
-  mount_uploader :image, ProductImageUploader
+  
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,8 @@ class Order < ActiveRecord::Base
 
   belongs_to :user
   has_many :line_items
-  mount_uploader :image, ProductImageUploader
+ 
   monetize :total_cents, numericality: true
-
   validates :stripe_charge_id, presence: true
 
 end
